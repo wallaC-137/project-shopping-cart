21fe03acb301483cb6ddb46361cb9df95e2eda5d
Wallace <wallace_031@hotmail.com>
2022-11-16 20:41:44 -0300

    4. Adicione um texto de carregando durante uma requisição à API

--- src/main.js
+++ src/main.js
@@ -7,10 +7,24 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const cPai = document.querySelector('.products');
 
+const loading = () => {
+  const elementLoad = document.createElement('p');
+  elementLoad.innerHTML = 'Loading...';
+  elementLoad.className = 'loading';
+  cPai.appendChild(elementLoad);
+};
+
 const AdicionaProdutos = async () => {
+  loading();
+
   const produtos = await fetchProductsList('computador');
-  produtos.forEach((elements) => {
-    cPai.appendChild(createProductElement(elements));
+  produtos.forEach((element, idx) => {
+    cPai.appendChild(createProductElement(element));
+
+    if (produtos.length - 1 === idx) {
+      cPai.firstChild.remove();
+    }
   });
 };
+
 AdicionaProdutos();
