e8c01764a5c26c5efc750de8e2dfd212b3bb83ce
Wallace <wallace_031@hotmail.com>
2022-11-16 19:19:28 -0300

    3. Crie uma listagem de produtos

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const cPai = document.querySelector('.products');
+
+const AdicionaProdutos = async () => {
+  const produtos = await fetchProductsList('computador');
+  produtos.forEach((elements) => {
+    cPai.appendChild(createProductElement(elements));
+  });
+};
+AdicionaProdutos();
